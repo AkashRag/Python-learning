b217d734198411c8db9e7c553a4a0841b1e57b00
AkashRag <qqqqq.raghuwanshi@gmail.com>
2026-05-24 17:03:39 +0530

    Day 3 - Real CRUD with student list - PATCH DELETE working

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+students = [
+    {"id": 1, "name": "Rahul", "city": "Delhi"},
+    {"id": 2, "name": "Priya", "city": "Mumbai"},
+    {"id": 3, "name": "Akash", "city": "Delhi"}
+]
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,30 +16,44 @@ def home():
 
 @app.get("/student")
 def get_student():
-    return{"name": "Rahul", "class": "12th", "city": "Delhi"}
+    return students
 
 
 @app.post("/student")
 
+#def add_student(data: dict):
+ #   name = data["name"]
+  #  city = data["city"]
+   # return {
+    #    "message": f"{name} from {city} added successfully!", "student" : data
 def add_student(data: dict):
-    name = data["name"]
-    city = data["city"]
+    students.append(data)
     return {
-        "message": f"{name} from {city} added successfully!", "student" : data
+        "message": f"Student added successfully!",
+        "student": data
+    }
     }
 
 # PATCH
 @app.patch("/student/{student_id}")
+def update_student(student_id: int, data: dict):
+    for student in students:
+        if student["id"] == student_id:
+            student.update(data)
+            return {
+                "message": f"Student {student_id} updated successfully!",
+                "updated student": student
+            }
+    return {"message": "Student not found!"}
 
-def update_student(student_id: int, data: dict) :
-    return  {
-        "message": f" Student {student_id} updated successfully!", "updated data": data
-    }
 
+#delete
 @app.delete("/student/{student_id}")
-
-def student_delete(student_id: int):
-    return{
-        "message": f"student {student_id} deleted succssesfully"
-
-    }
+def delete_student(student_id: int):
+    for student in students:
+        if student["id"] == student_id:
+            students.remove(student)
+            return {
+                "message": f"Student {student_id} deleted successfully!"
+            }
+    return {"message": "Student not found!"}
